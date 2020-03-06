9ff5ced0aa803c4da3c7603ad43385124ea4427f
Daniel Higuita <daniel.higuita007@gmail.com>
2020-03-06 00:17:32 -0500

    optimization

---
+++ wall/wall.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+
+var dimensions: Tuple
+
+func scale_wall() -> void:
+	var texture_dim: Vector2 = $Sprite.texture.get_size()
+	self.scale.x=self.dimensions.first_element/texture_dim.x
+	self.scale.y=self.dimensions.second_element/texture_dim.y
