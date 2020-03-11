bae91cde1b85b4781ecbb356b3bd9fcbbe032c61
Daniel Higuita <daniel.higuita007@gmail.com>
2020-03-10 19:24:26 -0500

    fixes

--- wall/wall.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody2D
-class_name Wall
-var dimensions: Tuple
-
-func scale_wall() -> void:
-	var texture_dim: Vector2 = $Sprite.texture.get_size()
-	self.scale.x=self.dimensions.first_element/texture_dim.x
-	self.scale.y=self.dimensions.second_element/texture_dim.y
